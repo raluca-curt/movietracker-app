bff482a176e15981f754f9f2f95e6d078a1f8966
Raluca Curt <ralucaioanacurt@gmail.com>
2022-12-07 20:30:58 +0200

    Updated api key

--- helper.py
+++ helper.py
@@ -10,7 +10,7 @@ def lookup_title(type, title):
     # Display a list of all movies with the same title
     if type == 'list':
         try:
-            response_raw = requests.get(f"http://www.omdbapi.com/?apikey={API_KEY}&s={title}&type=movie")
+            response_raw = requests.get(f"http://www.omdbapi.com/?apikey=5aa61ad0&s={title}&type=movie")
             
             response_raw.raise_for_status()
         except requests.exceptions.RequestException:
@@ -25,7 +25,7 @@ def lookup_title(type, title):
     # Display the first 
     elif type == 'search':
         try:
-            response_raw = requests.get(f"http://www.omdbapi.com/?apikey={API_KEY}&t={title}")
+            response_raw = requests.get(f"http://www.omdbapi.com/?apikey=5aa61ad0&t={title}")
             
             response_raw.raise_for_status()
         except requests.exceptions.RequestException:
@@ -42,7 +42,7 @@ def lookup_title(type, title):
 def lookup_id(movie_id):
     # movie_id = int(movie_id)
     try:
-        response_raw = requests.get(f"http://www.omdbapi.com/?apikey={API_KEY}&i={movie_id}")
+        response_raw = requests.get(f"http://www.omdbapi.com/?apikey=5aa61ad0&i={movie_id}")
         
         response_raw.raise_for_status()
     except requests.exceptions.RequestException:
